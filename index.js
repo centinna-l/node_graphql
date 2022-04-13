0bbed02caa97493ae820f3bc76d7afac5b87986c
centinna-l <jerry@logicwind.co>
2022-04-13 18:20:03 +0530

    Seq authenticate

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const server = require('./api/server');
+const server = require("./api/server");
+const { sequelize } = require("./database/models");
 
 const PORT = 8000;
 
-process.on('uncaughtException', (err) => {
-  console.error(`${(new Date()).toUTCString()} uncaughtException:`, err);
+process.on("uncaughtException", (err) => {
+  console.error(`${new Date().toUTCString()} uncaughtException:`, err);
   process.exit(0);
 });
 
-process.on('unhandledRejection', (err) => {
-  console.error(`${(new Date()).toUTCString()} unhandledRejection:`, err);
+process.on("unhandledRejection", (err) => {
+  console.error(`${new Date().toUTCString()} unhandledRejection:`, err);
 });
 
-
-server.listen(PORT, () => console.log(
-  `🚀 Server ready at http://localhost:${PORT}/api`,
-));
+server.listen(PORT, async () => {
+  console.log(`🚀 Server ready at http://localhost:${PORT}/api`);
+  await sequelize.authenticate();
+  console.log("DB Synced");
+});
